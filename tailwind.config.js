e7d0ee2dd4bbabc0e67d45eaacac1e055c01df19
Ali Hassan <i221545@nu.edu.pk>
2024-09-04 15:53:06 +0500

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      keyframes: {
+        rotateY: {
+          '0%': { transform: 'rotateY(0deg)' },
+          '100%': { transform: 'rotateY(360deg)' },
+        },
+      },
+      animation: {
+        'spin-y': 'rotateY 5s linear infinite',
       },
     },
   },
